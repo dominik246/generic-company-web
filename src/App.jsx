c1ab55b4b76207756ffcfadb934c8a6a90cc3caa
Dominik Jelicic <dominik246@gmail.com>
2021-04-07 18:09:48 +0200

    Added Pricing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/pages/Footer/Footer";
 import Home from "./components/pages/HomePage/Home";
+import Services from "./components/pages/Services/Services";
+import SignUp from "./components/pages/SignUp/SignUp";
+import Products from "./components/pages/Products/Products";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/services" component={Services} />
+        <Route path="/products" component={Products} />
+        <Route path="/sign-up" component={SignUp} />
       </Switch>
       <Footer />
     </Router>
